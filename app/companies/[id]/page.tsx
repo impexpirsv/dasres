d16f1f1500c31d46578d16f4879172cbe722c032
Siyamak Valizadeh <imp.exp.ir.sv@gmail.com>
2026-06-07 14:39:31 +0330

    Fix tsx import extensions

--- app/companies/[id]/page.tsx
+++ app/companies/[id]/page.tsx
@@ -1,5 +1,5 @@
 import { prisma } from "../../../lib/prisma";
-import DeleteCompanyButton from "../../components/DeleteCompanyButton.tsx";
+import DeleteCompanyButton from "../../components/DeleteCompanyButton";
 import Link from "next/link";
 export default async function CompanyProfilePage({
   params,
